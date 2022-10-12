de00aae419d8cab137ab28525494249f5f5bc11f
shadowfox22 <108629925+shadowfox22@users.noreply.github.com>
2022-10-11 21:01:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from LIBRARIES.functlb import *             # CONTAINS THE FUNCTIONS cl AND qrgen
+from LIBRARIES.importlb import *            # CONTAINS THE IMPORTS FOR THE PROGRAM
+
+cl()
+
+qrgen()
